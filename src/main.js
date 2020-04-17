fba445b4d41621aeee657a0357067a8c8aa31ff3
Kristina Davidovic <kristina.davidovic@agiledrop.com>
2020-04-17 17:31:11 +0200

    Add firebase, deploy app.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false;
 
 new Vue({
